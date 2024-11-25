db068e9d2d64392721cda2728aec66753c2629ac
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-25 16:27:16 -0400

    additional endpoint for Adding a group, group member, and fetching all groups for a specific member

--- src/API endpoints/API.tsx
+++ src/API endpoints/API.tsx
@@ -33,3 +33,48 @@ export const SignUpEndpoint = async (
     console.log(error);
   }
 };
+
+export const GetAllGroups = async (userID: string) => {
+  try {
+    const userGroups = await fetch(
+      `${import.meta.env.VITE_END_POINT}/groups/group_data?${userID}`
+    );
+    const response = await userGroups.json();
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const AddToGroupsEP = async (group_name: string) => {
+  try {
+    const userGroups = await fetch(`${import.meta.env.VITE_END_POINT}/groups`, {
+      method: "POST",
+      headers: { "Content-type": "application-json" },
+      body: JSON.stringify({ group_name }),
+    });
+    const response = await userGroups.json();
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const AddToGroupMembersEP = async (
+  group_id: string,
+  user_id: string
+) => {
+  try {
+    const userGroups = await fetch(
+      `${import.meta.env.VITE_END_POINT}/groups/group_member`,
+      {
+        method: "POST",
+        headers: { "Content-type": "application-json" },
+        body: JSON.stringify({ user_id, group_id }),
+      }
+    );
+    const response = await userGroups.json();
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
